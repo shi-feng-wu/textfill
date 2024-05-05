d6e0b54f15a9e0daea6938db0c13f4046ff6ceac
Shi Feng <shifengwu241@gmail.com>
2024-05-05 15:40:23 -0400

    added

---
+++ project/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    input_text = request.form['input_text']
+    output_text = f"Processed: {input_text}"
+    return render_template('index.html', input_text=input_text, output_text=output_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
